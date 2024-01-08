b442d66edac5f3dbc0aa0682edffd94230de60bf
parham <parham.mehrabi.webdev@gmail.com>
2024-01-08 03:42:21 +0330

    add new status tests for products

--- tests/integration/products/status.test.js
+++ tests/integration/products/status.test.js
@@ -12,8 +12,26 @@ describe('test products end-points response codes', () => {
     })
     
     describe('/api/v1/products/', () => {
-        it('Should return 200 for /', async () => {
+        beforeAll(() => {
+            // TODO: create 2 products and 2 categories before tests
+        })
+        it('GET / should return 200', async () => {
             const result = await request(server).get('/api/v1/products/');
+            expect(result.status).toBe(200);
+        })
+
+        it('GET /:id should return 200', async () => {
+            const result = await request(server).get('/api/v1/products/' + id);
+            expect(result.status).toBe(200);
+        })
+
+        it('POST / should return 201 for valid data', async () => {
+            const result = await request(server).post('/api/v1/products/');
+            expect(result.status).toBe(201);
+        })
+        
+        it('POST / should return 400 for Invalid data', async () => {
+            const result = await request(server).post('/api/v1/products/');
             expect(result.status).toBe(200);
         })
     })
