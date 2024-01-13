46b1a327450be0455fb910945a975ee63dbc14ad
parham <parham.mehrabi.webdev@gmail.com>
2024-01-13 21:37:30 +0330

    add User model

---
+++ models/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const { isEmail } = require('validator')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate: [isEmail, 'Invalid email format']
+    },
+    profilePicture: {
+        data: Buffer,
+        contentType: String,
+    },
+    address: [String],
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    isAdmin:  {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+userSchema.index({ email: 1 });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
