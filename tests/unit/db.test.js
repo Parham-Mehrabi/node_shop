983b2b37714cbb82253c62a4aede660d130e1d4d
parham <parham.mehrabi.webdev@gmail.com>
2024-01-17 20:20:45 +0330

    update db.test.js so it will load logger first

--- tests/unit/db.test.js
+++ tests/unit/db.test.js
@@ -5,6 +5,7 @@ describe('DataBase', () => {
         await mongoose.connection.close()
     })
     it('should connect to db', async () => {
+        await require('../../startup/logger.js')()
         await require('../../startup/db')()
         const state = mongoose.connection.readyState
         expect(state).toBe(1)
