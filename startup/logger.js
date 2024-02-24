f13b595d1ec0fd7495fe442f966f4ea5656201a7
parham <parham.mehrabi.webdev@gmail.com>
2024-02-24 21:33:46 +0330

    update logger.js
    now it only add console_logger when we are not in test environment

--- startup/logger.js
+++ startup/logger.js
@@ -1,5 +1,6 @@
 const winston = require('winston')
 
+const ENV = process.env.NODE_ENV
 
 module.exports = function () {
     const console_logger = new winston.transports.Console({
@@ -9,6 +10,8 @@ module.exports = function () {
         level: 'silly',
         filename: 'logfile.log'
     });
-    winston.add(console_logger);
+    if ( ENV !== 'test'){
+        winston.add(console_logger);
+    }
     winston.add(file_logger);
 }
