23e49ad6ff169815854572bf3007cc237a21cf7a
parham <parham.mehrabi.webdev@gmail.com>
2024-02-29 23:38:05 +0330

    update routes to pass more status tests

--- routes/API/v1/products_routs.js
+++ routes/API/v1/products_routs.js
@@ -6,4 +6,18 @@ products_router.get('/', (req, res) => {
     res.send('ok')
 })
 
+
+products_router.get('/:id', (req, res) => {
+    res.send('ok')
+})
+
+products_router.post('/', (req, res) => {
+    res.send('ok')
+})
+
+
+products_router.post('/:id', (req, res) => {
+    res.send('ok')
+})
+
 export default products_router
