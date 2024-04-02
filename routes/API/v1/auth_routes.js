e95d9b58b8db9178417a639210ee77211f3ab1dc
parham <parham.mehrabi.webdev@gmail.com>
2024-04-02 22:10:52 +0330

    starting authentication

---
+++ routes/API/v1/auth_routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+const auth_routes = express.Router({mergeParams: true});
+
+
+// TODO
+
+// register (create_user)
+
+// authentication (get_token)
+
+// authorization (verify_token)
+
+// logout (revoke_token)
+
+
+export default auth_routes;
--- startup/router.js
+++ startup/router.js
@@ -1,10 +1,11 @@
-import products_routes from '../routes/API/v1/products_routs.js'
-import users_router from '../routes/API/v1/users_routs.js'
+import products_routes from '../routes/API/v1/products_routs.js';
+import users_router from '../routes/API/v1/users_routs.js';
+import auth_routes from '../routes/API/v1/auth_routes.js';
 
 export default function(app){
     
-    // TODO: add routes here
     app.use('/api/v1/products/', products_routes);
     app.use('/api/v1/users/', users_router);
+    app.use('/api/v1/auth/', auth_routes);
 };
 
