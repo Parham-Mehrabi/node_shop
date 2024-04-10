375763a2a00cac9744a9ce9ab65cd65a183b765c
parham <parham.mehrabi.webdev@gmail.com>
2024-04-10 21:10:36 +0330

    add register user end point

--- routes/API/v1/auth_routes.js
+++ routes/API/v1/auth_routes.js
@@ -1,12 +1,29 @@
 import express from 'express';
-
+import User from '../../../models/users.js';
+import Joi from 'joi';
 
 const auth_routes = express.Router({mergeParams: true});
 
 
-// TODO
+const EmailSchema = Joi.object({
+    email: Joi.string().email().required().lowercase(),
+})
 
 // register (create_user)
+auth_routes.post('/register', async (req, res) => {
+
+    // validate user's input
+    const data = EmailSchema.validate(req.body);
+    // return error if input have problem
+    if (data.error) return res.status(400).send(data.error);
+    
+    const user = new User(data.value);
+    console.log(data.value)
+    console.log(req.body)
+    user.save();
+    res.status(201).send(`${user.email} created`);
+
+})
 
 // authentication (get_token)
 
