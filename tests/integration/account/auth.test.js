0893a1f42bf5d5538e58934eda86246a97f13136
parham <parham.mehrabi.webdev@gmail.com>
2024-04-12 16:36:57 +0330

    add tests for authentication
    add a test for register end point

---
+++ tests/integration/account/auth.test.js
@@ -0,0 +1,45 @@
+import request from 'supertest';
+import Chance from 'chance';
+import User from '../../../models/users';
+import Server from '../../../index';
+import db from '../../../startup/db.js';
+import logger from '../../../startup/logger.js';
+
+describe("test Authentication", () => {
+    let endPoint = '/api/v1/auth/'
+    let Faker = new Chance
+    let server;
+
+    beforeAll(async () => {
+        server = await Server()
+        logger()
+        await db()
+    })
+    afterAll(async () => {
+        await server.close()
+    })
+
+    it("register a new user", async () => {
+        let email = Faker.email()
+        let data = {
+            "email": email
+        }
+        let result = await request(server)
+            .post(endPoint + 'register')
+            .send(data)
+        let user = await User.findOne({email:email})
+
+        expect(user).toBeTruthy()
+        expect(result.statusCode).toBe(201)
+    })
+
+
+    it("should return a valid token for given user", () => {
+
+    })
+
+    it("should identify a logged in user", () => {
+
+    })
+
+})
