2fb4b007438429e477e07fba7ff780da94c2f940
parham <parham.mehrabi.webdev@gmail.com>
2024-04-17 15:21:41 +0330

    add login route to get a token

--- routes/API/v1/auth_routes.js
+++ routes/API/v1/auth_routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import User from '../../../models/users.js';
 import Joi from 'joi';
+import bycrypt from 'bcrypt';
 
 const auth_routes = express.Router({mergeParams: true});
 
 
-const EmailSchema = Joi.object({
+const UserFormSchema = Joi.object({
     email: Joi.string().email().required().lowercase(),
     password: Joi.string().required()
     // FIXME: validate password complexity
@@ -15,7 +16,7 @@ const EmailSchema = Joi.object({
 auth_routes.post('/register', async (req, res) => {
 
     // validate user's input
-    const data = EmailSchema.validate(req.body);
+    const data = UserFormSchema.validate(req.body);
     // return error if input have problem
     if (data.error) return res.status(400).send(data.error);
     
@@ -26,8 +27,20 @@ auth_routes.post('/register', async (req, res) => {
 })
 
 // authentication (get_token)
-auth_routes.post('/login', async() => {
-    
+auth_routes.post('/login', async(req, res) => {
+    const data = UserFormSchema.validate(req.body);
+    if (data.error) return res.status(400).send(data.error)
+
+    const user = await User.findOne({email: data.value.email});
+    // check if user with that email exist
+    if (!user) return res.status(400).send("Invalid Email or Password")
+
+    const ValidPassword = await bycrypt.compare(data.value.password, user.password)
+    // check if password matched
+    if (!ValidPassword) return res.status(400).send("Invalid Email or Password")
+
+    const token = user.generateAuthToken()
+    return res.json({token: token})
 })
 
 // authorization (verify_token)
