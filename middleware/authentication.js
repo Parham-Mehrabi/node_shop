ab25a9f8376cebd27291551a9d7aee746b9e1ef9
parham <parham.mehrabi.webdev@gmail.com>
2024-04-22 21:18:57 +0330

    update and fix auth middleware
    + now it reject invalid signatures
    + now it sets a message for anonymous users

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,17 +1,19 @@
 import jwt from 'jsonwebtoken';
 import config from 'config';
 
-export default function(req, res, next) {
+export default async function (req, res, next) {
     const token = req.headers.jwt_token
-    if (token){
+    req.user = {name: "anonymous", message: "unauthorized"}
+    if (token) {
         const user = jwt.decode(token, config.get("JWT_SECRET"))
-        if(user){
-            req.user = user;
-        }else{
-            req.user = "anonymous"
+        if (user) {
+            try {
+                jwt.verify(token, config.get("JWT_SECRET"))
+                req.user = user;
+            } catch (e) {
+                req.user.message = e.message
+            }
         }
-    }else{
-        req.user = "anonymous"
     }
     next()
 }
