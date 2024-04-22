7764289eaa4112c9cb1dc12912836d40f554b220
parham <parham.mehrabi.webdev@gmail.com>
2024-04-22 21:17:59 +0330

    add more tests for auth middle ware
    add tests for invalid tokens and expired tokens
    also expect middleware to set a message for anonymous users

--- tests/unit/middlewares/auth.test.js
+++ tests/unit/middlewares/auth.test.js
@@ -2,10 +2,14 @@ import authentication from "../../../middleware/authentication"
 import User from '../../../models/users'
 import mongoose from "mongoose";
 import {jest} from '@jest/globals'
+import Jwt from "jsonwebtoken"
+import config from "config"
+
 
 describe("testing authentication middle", () => {
     const user = new User({
         email: "auth@middleware.com",
+        name: "auth_user",
         _id: new mongoose.Types.ObjectId().toHexString(),
     })
 
@@ -24,7 +28,50 @@ describe("testing authentication middle", () => {
         const req = {headers: {}}      // request without token
         const next = jest.fn()
         authentication(req, res, next)
-        expect(req.user).toBe("anonymous")
+
+        expect(req.user.name).toBe("anonymous")
+        expect(req.user.message).toBe("unauthorized")
     })
+
+
+    it("should set user to anonymous for Invalid token Signature", () => {
+        const token = Jwt.sign(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin
+            },
+            "INVALID_SECRET_KEY"
+        )
+        const res = {}
+        const req = {headers: {jwt_token: token}}      // request without token
+        const next = jest.fn()
+        authentication(req, res, next)
+
+        expect(req.user.name).toBe("anonymous")
+        expect(req.user.message).toBe("invalid signature")
+    })
+
+    it("should set user to anonymous expired tokens", () => {
+        const token = Jwt.sign(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin
+            },
+            config.get("JWT_SECRET"),
+            { expiresIn: 0 }
+        )
+        const res = {}
+        const req = {headers: {jwt_token: token}}      // request without token
+        const next = jest.fn()
+        authentication(req, res, next)
+
+        expect(req.user.name).toBe("anonymous")
+        expect(req.user.message).toBe("jwt expired")
+    })
+
     
 })
